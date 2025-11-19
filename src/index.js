303cf985a41f15346123ae76e013d16fb1a9a107
Denisath <denisathaullahdenis@gmail.com>
2025-11-19 09:18:27 +0700

    get by id

--- src/index.js
+++ src/index.js
@@ -4,27 +4,43 @@ const app = express();
 app.use(express.json());
 
 //Dummy data
-let users = [
+let dataDummy = [
     { id: 1, name: "ucup" },
     { id: 2, name: "rucup" },
 ];
 
 //method get
 app.get("/users", (req, res) =>{
-    res.json(users);
+    res.json(dataDummy);
 });
 
+//method get by id
+app.get("/users/:id", (req, res) => {
+    const {id} = req.params;
+    const userId = dataDummy.find((item) => item.id == id);
+    console.log(userId);
+    if(userId){
+        res.status(200).json({msg: "succsess", data: userId});
+    }
+
+    if(!userId){
+        res.status(404).json({msg: "user tidak ditemukan"})
+    };
+})
+
+
 //method post
-app.post("/users", (req, res) =>{
+app.post("/users", (req, res) => {
     const { name } = req.body;
-    const newUser = { id: users.length + 1, name:name };
-    users.push(newUser)
+    const newUser = { id: dataDummy.length + 1, name:name };
+    dataDummy.push(newUser)
     res.status(201).json(newUser);
 });
 
+app.put("/users/:id", (req, res) => {
 
-app.get("/tes", (req, res) => {
-    res.send("selamat datang di sistem saya...");
 })
+
+
 
 app.listen(5001, () => console.log("Server running on http://localhost:5001"))
